fa383dc08dcd52d4dd666b6734729688d8464a72
cro2003 <chiragrai2003@icloud.com>
2025-01-01 19:30:36 +0530

    Added Readme

--- frontend/src/components/services/api.js
+++ frontend/src/components/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "http://127.0.0.1:5000/api", // Update with your backend's URL
   headers: {
     "Content-Type": "application/json",
   },
